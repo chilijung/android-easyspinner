611e3a74c8b1279172be6a5b656bf313d19b9786
howardchi <howardchi@fougood.com>
2017-06-16 14:33:25 +0800

    fix layout and add styles

--- spinnercollection/src/main/java/io/canner/spinnercollection/LeftRightSpinner.java
+++ spinnercollection/src/main/java/io/canner/spinnercollection/LeftRightSpinner.java
@@ -2,11 +2,14 @@ package io.canner.spinnercollection;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,9 +19,11 @@ import io.canner.spinnercollection.adapter.NothingSelectedSpinnerAdapter;
  * Created by lijung on 6/16/17.
  */
 
-public class LeftRightSpinner extends LinearLayout {
+public class LeftRightSpinner extends RelativeLayout {
     private String title = "";
     private int entriesResId;
+    private int titleStyleResId;
+    private int spinnerMode;
 
     public LeftRightSpinner(Context context ) {
         this(context, null);
@@ -30,10 +35,11 @@ public class LeftRightSpinner extends LinearLayout {
 
     public LeftRightSpinner(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
         TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.LeftRightSpinner);
         CharSequence title = arr.getString(R.styleable.LeftRightSpinner_title);
         int entriesResId = arr.getResourceId(R.styleable.LeftRightSpinner_entries, 0);
+        int titleStyleResId = arr.getResourceId(R.styleable.LeftRightSpinner_titleTextStyle, R.style.innerText);
+        int spinnerMode = arr.getInt(R.styleable.LeftRightSpinner_spinnerMode, 0);
 
         if (title != null) {
             this.title = title.toString();
@@ -43,17 +49,41 @@ public class LeftRightSpinner extends LinearLayout {
             this.entriesResId = entriesResId;
         }
 
+        this.titleStyleResId = titleStyleResId;
+        this.spinnerMode = spinnerMode;
+
         arr.recycle();  // Do this when done.
         init();
     }
 
     private void init() {
-        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.spinner_left_right_form, this);
-        TextView titleView = (TextView) rootView.findViewById(R.id.spinner_form_title);
-        Spinner spinnerView = (Spinner) rootView.findViewById(R.id.spinner_form_choose);
+        TextView titleView = new TextView(getContext());
+        Spinner spinnerView = new Spinner(getContext(), spinnerMode);
 
+        // setup title view and add to relativelayout.
         titleView.setText(title);
+        if (Build.VERSION.SDK_INT < 23) {
+            titleView.setTextAppearance(getContext(), titleStyleResId);
+        } else {
+            titleView.setTextAppearance(titleStyleResId);
+        }
+
+        RelativeLayout.LayoutParams titleLayoutParams = new RelativeLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT
+        );
+        titleLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        titleView.setLayoutParams(titleLayoutParams);
+        addView(titleView);
+
+        // setup spinner view and add to view
         spinnerView.setPrompt(title);
+        RelativeLayout.LayoutParams spinnerLayoutParams = new RelativeLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT
+        );
+        spinnerLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        spinnerView.setLayoutParams(spinnerLayoutParams);
         ArrayAdapter<CharSequence> spinnerArrayAdapter = ArrayAdapter.createFromResource(getContext(), entriesResId, R.layout.spinner_item_selected);
         spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
         spinnerView.setAdapter(
@@ -63,5 +93,6 @@ public class LeftRightSpinner extends LinearLayout {
                         getContext()
                 )
         );
+        addView(spinnerView);
     }
 }
